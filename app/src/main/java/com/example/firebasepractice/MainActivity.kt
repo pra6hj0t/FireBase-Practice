5405116f3d693c58d86e93566c6b10bacb31f6ed
pra6hj0t <157087592+pra6hj0t@users.noreply.github.com>
2024-03-02 08:44:53 +0530

    data

--- app/src/main/java/com/example/firebasepractice/MainActivity.kt
+++ app/src/main/java/com/example/firebasepractice/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.util.Patterns
 import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioButton
@@ -12,6 +13,7 @@ import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import java.util.regex.Pattern
 
 class MainActivity : AppCompatActivity() {
     lateinit var DataBase : DatabaseReference
@@ -58,6 +60,13 @@ class MainActivity : AppCompatActivity() {
             if (Email.trim().equals("")){
                 email.setError("Email is required")
                 return@setOnClickListener
+            }else{
+                if(Patterns.EMAIL_ADDRESS.matcher(Email).matches()){
+
+                }else{
+                    email.setError("Enter Valid Email")
+                    return@setOnClickListener
+                }
             }
             if (Password.trim().equals("")){
                 password.setError("Password is required")
